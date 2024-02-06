a9833ff0a94dd9012bb371a304c83ffd5b5ba870
bishwarup307 <15094759+bishwarup307@users.noreply.github.com>
2024-02-07 00:09:45 +0530

    Add keys to the calculator

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
-const container = document.querySelector(".container");
+const DIVIDE_TEXT = "&#xF7";
+const MULTIPLY_TEXT = "&times";
+
+const keysContainer = document.querySelector(".keys-container");
+const display = document.querySelector(".display-container");
+
+const SYMBOLS = [
+    { text: "AC", id: "ac" },
+    { text: "+/-", id: "sign" },
+    { text: "%", id: "percent" },
+    { text: DIVIDE_TEXT, id: "divide" },
+    { text: 7 },
+    { text: 8 },
+    { text: 9 },
+    { text: MULTIPLY_TEXT, id: "multiply" },
+    { text: 4 },
+    { text: 5 },
+    { text: 6 },
+    { text: "-", id: "minus" },
+    { text: 1 },
+    { text: 2 },
+    { text: 3 },
+    { text: "+", id: "plus" },
+    { text: 0 },
+    { text: ".", id: "decimal" },
+    { text: "=", id: "equal" },
+];
+
+const OPERATORS = ["plus", "minus", "multiply", "divide", "equal"];
+const OTHERS = ["ac", "sign", "percent"];
 
 function makeKeyGrid() {
-  const placeHolder = document.createDocumentFragment();
+    const placeHolder = document.createDocumentFragment();
 
-  let key;
-  for (let i = 1; i <= 9; i++) {
-    key = document.createElement("div");
-    key.classList.add("number-key");
-    key.id = $`key-{i}`;
-    key.textContent = i;
-    placeHolder.appendChild(key);
-  }
+    // styles = getComputedStyle(document.body)
 
-  acKey = document.createElement("div");
-  acKey.id = "ac-key";
+    // const displayHeight = getComputedStyle(display).getPropertyValue("height")
 
-  signKey = document.createElement("div");
-  signKey.id = "sign-key";
+    // const allocatedWidth = styles.getPropertyValue("--calc-width")
+    // const allocatedHeight = styles.getPropertyValue("--calc-height") - displayHeight;
 
-  percentKey = document.createElement("div");
-  percentKey.id = "percent-key"[("U+00F7", "U+00D7", "-", "+", "=")].forEach(
-    (operator) => {}
-  );
+    // const cellWidth = allocatedWidth / 4    // 4 columns per row
+    // const cellHeight = allocatedHeight / 5  // 5 rows per column
+
+    SYMBOLS.forEach((symbol) => {
+        key = document.createElement("div");
+        key.innerHTML = symbol.text;
+        key.classList.add("key");
+        if (!symbol.id) {
+            key.classList.add("number-key");
+            symbol.id = `${symbol.text}`;
+        }
+
+        if (symbol.id === "decimal") key.classList.add("number-key");
+
+        if (OPERATORS.includes(symbol.id)) key.classList.add("operator-key");
+        if (OTHERS.includes(symbol.id)) key.classList.add("other-key");
+
+        key.id = `key-${symbol.id}`;
+        if (symbol.text === 0) key.classList.add("zero-key");
+        placeHolder.appendChild(key);
+    });
+
+    keysContainer.appendChild(placeHolder);
 }
+
+makeKeyGrid();
